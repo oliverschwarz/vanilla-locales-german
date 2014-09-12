8e8aa5e12bfd30a9897ec5179e4de2b61e37f637
Oliver Schwarz <Oliver Schwarz@192.168.2.101>
2014-09-12 14:34:53 +0200

    Adding definition file for integrating the german locale into Vanilla

---
+++ definitions.php
@@ -0,0 +1,28 @@
+<?php if (!defined('APPLICATION')) exit();
+
+/**
+ * German locale for the Vanilla forum software
+ * 
+ * A full translation to German including all default plugins and
+ * admin as of 2.0.17.10. This has mainly been done by Matthias Niess
+ * in 2011. I am just taking over his great work and add the translations
+ * for recent Vanilla version changes.
+ * 
+ * @author  Matthias Niess <mniess at gmail dot com>
+ * @author  Oliver Schwarz <oliver at eanna dot de>
+ * @license The MIT License (MIT)
+ */
+
+/**
+ * German locale description for Vanilla
+ */
+$LocaleInfo['german'] = array (
+    'Locale' => 'de-DE',
+    'Name' => 'German Locale',
+    'Description' => 'A full translation to German including all default plugins and admin as of 2.0.17.10. Most of the work has been done by mniess at gmail dot com. Thank you!',
+    'Version' => '1.1.0',
+    'Author' => 'Oliver Schwarz',
+    'AuthorEmail' => 'oliver at eanna dot de',
+    'AuthorUrl' => 'http://eanna.de',
+    'License' => 'The MIT License (MIT)'
+);
